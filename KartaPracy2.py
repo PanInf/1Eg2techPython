15a73101c718d36b8d3e99edf96f942d9ff284f1
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-03 12:15:35 +0000

    Dodano zad 1-2 z Karty Pracy 2

---
+++ KartaPracy2.py
@@ -0,0 +1,16 @@
+# Zad 1
+
+# a = int(input())
+# if a % 3 == 0:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# Zad 2
+
+a = int(input())
+if a >= 100 and a < 1000 and a % 17 == 0:
+    print("TAK")
+else:
+    print("NIE")
+
