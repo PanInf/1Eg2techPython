b982ccb74a0a12f1bbd3584f185d3c88b84e5f10
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-05 06:03:23 +0000

    Ukończono KartaPracy2.pdf

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -8,9 +8,51 @@
 
 # Zad 2
 
-a = int(input())
-if a >= 100 and a < 1000 and a % 17 == 0:
-    print("TAK")
-else:
-    print("NIE")
+# a = int(input())
+# if a >= 100 and a < 1000 and a % 17 == 0:
+#     print("TAK")
+# else:
+#     print("NIE")
 
+# Zad 3
+
+# wiek = int(input("Ile masz lat? "))
+# if wiek >= 18:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# Zad 4
+
+# LIMIT = 20
+# waga = int(input("Ile to barachło waży? "))
+# if waga <= LIMIT:
+#     print("TAK, możesz wjechać")
+# else:
+#     print("NIE, lepiej nie wjeżdżaj")
+
+# Zad 5
+
+# a, b, c = map(int, input().split())   # int(input().split())) NIE DZIALA
+# if (a < c and b > c) or (b < c and a > c):
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# Zad 6
+
+# a = int(input("Podaj dowolną liczbę "))
+# p = int(input("Podaj liczbę pierwszą "))
+# if ((a**p - a) % p) == 0:
+#     print("TAK, spełnia MTF")
+# else:
+#     print("NIE, nie spełnia MTF")
+    
+# Zad 7
+
+# p, k, s = map(int, input().split())
+# if 3*s + p >= k:
+#     print("TAK")
+# else:
+#     print("NIE")
+        
