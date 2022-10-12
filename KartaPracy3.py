1fda2c4e9d0c9ed78e8b0b1ab775f10f79957514
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-12 05:20:10 +0000

    Dodanie tymczasowe KartaPracy3.py

---
+++ KartaPracy3.py
@@ -0,0 +1,28 @@
+# Ankieta
+# User wprowadza dwie liczby. 
+# Sprawdz czy druga jest 
+# o ponad 30% większa niż pierwsza
+# WE: p, q
+# WY: TAK/NIE
+
+# p, q = int(input()), int(input())
+# if (1.3*p > q):
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# pętla liczb dwucyfrowych od 10 do 21
+
+# pętla liczb dwucyfrowych nieparzystych od 15 do 31  
+
+# pętla liczb dwucyfrowych malejąco (step ujemny)
+
+# pętla liczb dwucyfrowych malejąco (step dodatni)
+
+# pętla liczb 3-cyfrowych podzielnych przez 20
+
+
+
+
+
+
