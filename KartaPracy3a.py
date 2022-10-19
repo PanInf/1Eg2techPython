0f9473d61d5d7d1ad287cdf1bb27af5a2ac4f8d0
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-19 06:08:43 +0000

    Wykonano KartaPracy3a (zad 1-2)

---
+++ KartaPracy3a.py
@@ -0,0 +1,20 @@
+# Zad 1
+# n = int(input())
+
+# for i in range(n):
+#     print("*-|", end="")
+
+# Zad 2 PRE
+
+# print("Bartosz"*2)
+# print("*"*5)
+
+# Zad 2
+
+# n = int(input())
+# for i in range(1, n+1):
+#     print("*"*i, end="")
+#     if i%2 == 1:
+#         print("||",end="")
+#     else:
+#         print("--",end="")
