ea95105e215a1a62810f685cf31a6cd264f80a66
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-24 12:28:50 +0000

    KartaPracy3a - Zad3

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -18,3 +18,23 @@
 #         print("||",end="")
 #     else:
 #         print("--",end="")
+
+# Zad 3
+
+# n = int(input())
+# for i in range(1, n+1):
+#     print("*", end="")
+#     if i % 2 == 1:
+#         print("|"*i, end="")
+#     else:
+#         print("-"*i, end="")
+
+# PRE - Tabliczka mnożenia
+
+for i in range(1,11):
+    for j in range(1,11):
+        print(i*j, end="\t\t")
+    print()
+    print()
+
+
