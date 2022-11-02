9974c159c2208d7fc90a3bf1a13217704d047027
PanInf <77333045+PanInf@users.noreply.github.com>
2022-11-02 06:30:01 +0000

    Dodanie zadań PRE

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -31,10 +31,34 @@
 
 # PRE - Tabliczka mnożenia
 
-for i in range(1,11):
-    for j in range(1,11):
-        print(i*j, end="\t\t")
-    print()
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(i*j, end="\t")
+#     print()
+
+# PRE na 2 pętle:
+
+# *
+# **
+# ***
+# ****
+
+# ****
+# ***
+# **
+# *
+
+n = int(input())
+
+for i in range(n):
+    for j in range(i+1):
+        print("*", end="")
     print()
 
+print()
+print()
 
+for i in range(n):
+    for j in range(n-i):
+        print("*", end="")
+    print()
