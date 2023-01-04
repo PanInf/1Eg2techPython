fa6ded12d52fabff049b0bc177a5887dd1816772
PanInf <77333045+PanInf@users.noreply.github.com>
2023-01-04 13:17:23 +0000

    Dodanie Pseudokodu

--- KartyPracy/KartaPracy6a.py
+++ KartyPracy/KartaPracy6a.py
@@ -33,15 +33,46 @@
 
 # Zad 6
 
-a = int(input())
-b = int(input())
+# a = int(input())
+# b = int(input())
 
-x, y = a, b
-while x!=y:
-    if x>y: x -= y
-    if y>x: y -= x
+# x, y = a, b
+# while x!=y:
+#     if x>y: x -= y
+#     if y>x: y -= x
 
-c = a//x
-d = b//x
+# c = a//x
+# d = b//x
 
-print(f"{a}/{b} = {c}/{d}")
+# print(f"{a}/{b} = {c}/{d}")
+
+# Zad 8
+# T = []
+# for i in range(2,10001):
+#     suma1 = 0
+#     for j in range(1,i):
+#         if i % j == 0:
+#             suma1 += j
+#     T.append(suma1)
+#     suma2 = 0
+#     for k in range(1,suma1):
+#         if suma1 % k == 0:
+#             suma2 += k
+#     if i == suma2 and suma1 != suma2 and suma2 not in T:
+#         print(f"({suma1},{suma2})")
+
+# Zad 9
+
+# def czy_pierwsza(n):
+#     for i in range(2,n):
+#         if n % i == 0:
+#             return False
+#     return True
+
+# for i in range(10,100):
+#     for j in range(2,i):
+#         if i % j == 0:
+#             if czy_pierwsza(j) and czy_pierwsza(i//j):
+#                 print(i, end=" ")
+#                 break
+
